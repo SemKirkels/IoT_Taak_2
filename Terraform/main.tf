5a332095d655a3aabc6ff104f07a9b9ea47fb798
Sem Kirkels <Sem.kirkels@student.pxl.be>
2022-10-05 12:16:19 +0200

    Upload main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "ghcr.io/semkirkels/iot_taak_2/taak_2_docker_image:sha-e84ca8c"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "iot_taak_2"
+  must_run = false
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
